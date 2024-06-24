ad75bf70ecbeabc1ba64ef3e13a35d7d855e7d3a
Eklavya163940 <eklavya.agal@einfochips.com>
2024-06-24 10:32:33 +0530

    commit

---
+++ Monty_pyhton.py
@@ -0,0 +1,12 @@
+#In a .py file, create a program and use input() three times to get answers to the following questions from a user.  Store each of the answers in a variable.
+
+#What is your name?
+var_name = input("What is your name?")
+
+#What is your quest?
+var_quest = input("What is your quest?")
+
+#What is your favorite color?
+var_color = input("What is your favorite color?")
+
+print("So your name is " + name + ", your quest is " + quest + ", and your favorite color is " + color + ".")
